28f6150c02b5e3a515fbf74cb94b0a27064f2860
andreyshu <shoodria@gmail.com>
2020-07-17 21:20:01 +0900

    add player movement

--- Game_002_sfml/Player.cpp
+++ Game_002_sfml/Player.cpp
@@ -2,7 +2,7 @@
 
 void Player::initVariables()
 {
-
+	this->movementSpeed = 10.f;
 }
 
 void Player::initShape()
@@ -11,8 +11,10 @@ void Player::initShape()
 	this->shape.setSize(sf::Vector2f(50.f, 50.f));
 }
 
-Player::Player()
+Player::Player(float x, float y)
 {
+	this->shape.setPosition(x, y);
+
 	this->initVariables();
 	this->initShape();
 }
@@ -22,9 +24,37 @@ Player::~Player()
 
 }
 
-void Player::update()
+void Player::updateInput()
 {
+	//Keyboard
+	//Left
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		this->shape.move(-this->movementSpeed, 0.f);
+	}
+	//Right
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		this->shape.move(this->movementSpeed, 0.f);
+	}
+	//Up
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+	this->shape.move(0.f, -this->movementSpeed);
+	}
+	//Down
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+	this->shape.move(0.f, this->movementSpeed);
+	}
 
+}
+
+void Player::update(sf::RenderTarget* target)
+{
+	//Window bounds collision
+
+	this->updateInput();
 }
 
 void Player::render(sf::RenderTarget* target)
